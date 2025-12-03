112b629f173fae8fcefca312a864a7d3930dc02b
IkramullahMazhar <2024476@student.cct.ie>
2025-12-03 21:23:32 +0000

    Updates Files form.html & Server.js To receive data
    Have edited the files and now it receives the data entered in the form and shows it on the terminal that data has been received , thanks !

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+// server.js
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
-// Serve static files
+// This lets Express read form data from POST requests (req.body)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from the current folder (so form.html can be served)
 app.use(express.static('.'));
 
-// Route for the main page
+// Route for the main page - sends form.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/form.html');
+    res.sendFile(path.join(__dirname, 'form.html'));
+});
+
+// Route that receives the form submission
+app.post('/submit', (req, res) => {
+    // req.body will contain all form fields
+    console.log('Received form data:', req.body);
+
+    // For now, just send a simple response
+    res.send('Form data received on the server');
 });
 
 // Start the server
