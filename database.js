fa79f99df11a28e4a30f3bbf62a9bc25dd038fe2
IkramullahMazhar <2024476@student.cct.ie>
2025-12-04 20:18:34 +0000

    updated database connected to mysql
    this right here stores the data entered in the form and then tends to store it in mysql in the tables

--- database.js
+++ database.js
@@ -1,3 +1,31 @@
-// This file will connect to your database
-// For now, leave it empty or add a comment
-console.log('Database connection setup will go here');
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',           // or your username
+    password: '356D0278i!', // the same password you used in Workbench
+    database: 'assignment_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+async function checkDatabaseConnection() {
+    try {
+        const connection = await pool.getConnection();
+        console.log('Connected to MySQL database.');
+
+        await connection.query('SELECT 1 FROM mysql_table LIMIT 1;');
+        console.log('mysql_table exists and is accessible.');
+
+        connection.release();
+    } catch (err) {
+        console.error('Database connection or schema error:', err.message);
+        throw err;
+    }
+}
+
+module.exports = {
+    pool,
+    checkDatabaseConnection
+};
